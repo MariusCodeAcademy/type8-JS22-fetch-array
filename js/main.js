80c3ef5dea0a6f9492ef99bbb141fa2ad9a922dc
Marius <marius@example.com>
2022-03-24 08:45:59 +0200

    users

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log('main');
+// nusitaikom i mygtuka ir app el
+
+// mygtuko paspaudimu parsisiusti visus vartotojus
+
+// is gautu duomenu sugeneruoti li elementus (button)
+
+// sugeneruoti ol el, ir sudeti i ji li elementus. ol el patalpini appEl
+
+// mygtuko 'read more' paspaudimu parsiunciam tik nurodyto vartotojo info
+// atvaizduojam papildomoj korteleje virs saraso
+// 1. paspaudus mygtuka atspausdinkite to vartotojo id ant kurio paspausta.
+// 2. turedami id parisiuskite su papildomu fetch info apie ta vartotoja
+// 3. sukurti ir supildyti vartotojo korteles info.
+// 4. atvaizduojam varda, addreso, gatve, miesta ir zip koda, telefona ir catchphrase
